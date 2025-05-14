0518aa56f159667ac9f20cbade7727addb1ac316
AroojiNO <noaharooji@gmail.com>
2025-05-14 19:36:15 -0400

    pages name

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,8 +8,8 @@ export default function Navbar() {
   const pathname = usePathname();
   const links = [
     { href: "/", label: "Home" },
-    { href: "/resume", label: "Resume" },
-    { href: "/papers", label: "Papers" },
+    { href: "/resume", label: "resume" },
+    { href: "/papers", label: "papers" },
   ];
 
   return (
